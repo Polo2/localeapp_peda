5948891aaea05d8a45bcf414197293041eb702df
Paul M2B <polo2bodi@gmail.com>
2018-07-02 10:24:32 +0200

    reset and hide APi key

--- config/initializers/localeapp.rb
+++ config/initializers/localeapp.rb
@@ -2,5 +2,4 @@ require "localeapp/rails"
 
 Localeapp.configure do |config|
   config.api_key = ENV['LOCALEAPP_API_KEY']
-  # config.api_key = "MoksdSr5XZbH5vGYY70dPQbDnYA7OjJu0wWZNhsgQYdSiw3LtL"
 end
